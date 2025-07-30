466683bb7756b661cd37a8bb00b760003d5c05fc
Dastan Torokul <torokul@yahoo.com>
2025-07-30 17:46:50 +0600

    Change test.yml file

--- app/src/test/java/org/example/Pokemon.java
+++ app/src/test/java/org/example/Pokemon.java
@@ -1,4 +1,18 @@
 package org.example;
 
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.Test;
+
 public class Pokemon {
+    @Test
+    public void getPokemonsTest() {
+        Response response = RestAssured.given().header("Accept", "application/json")
+                .when().get("https://pokeapi.co/api/v2/pokemon")
+                .then().statusCode(200).extract().response();
+        PokemonPojo deserializedRsponse = response.as(PokemonPojo.class);
+
+        System.out.println(deserializedRsponse.getCount());
+        System.out.println(deserializedRsponse.getNext());
+    }
 }
